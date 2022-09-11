11c3ef6ae253847b80d63a13cf7cb13fb152523a
Arnas <worgenmagic@gmail.com>
2022-09-11 10:08:48 +0300

    CRUD Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,13 @@ class Post extends Model
     protected $table = 'posts';
     protected $guarded = false;
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'post_tags', 'post_id', 'tag_id');
+    }
 }
